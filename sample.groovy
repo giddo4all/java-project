867c8aa5cff4eba810d73c25146f26f3d3fb5105
giddo4all <gideonaina@yahoo.com>
2018-05-03 20:15:05 -0500

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,12 +1,4 @@
 
-def getBuildStatus() {
-    def stat = currentBuild.currentResult
-    if (currentBuild?.getPreviousBuild()?.resultIsWorseOrEqualTo('FAILURE') && currentBuild.currentResult == "SUCCESS") {stat = 'BUILD_FIXED'}
-    if (currentBuild?.getPreviousBuild()?.resultIsWorseOrEqualTo('FAILURE') && currentBuild.currentResult == "FAILURE") {stat = 'STILL_FAILING'}
-    if (currentBuild?.getPreviousBuild()?.result == 'UNSTABLE' && currentBuild.currentResult == "SUCCESS") {stat = 'BUILD_STABLE'}
-    return stat
-}
-
 def slackMsg(){
 
     echo pwd()
@@ -15,3 +7,12 @@ def slackMsg(){
     slackSend color: '#00FF00', message: getBuildStatus()
 }
 return this
+
+def getBuildStatus() {
+    def stat = currentBuild.currentResult
+    if (currentBuild?.getPreviousBuild()?.resultIsWorseOrEqualTo('FAILURE') && currentBuild.currentResult == "SUCCESS") {stat = 'BUILD_FIXED'}
+    if (currentBuild?.getPreviousBuild()?.resultIsWorseOrEqualTo('FAILURE') && currentBuild.currentResult == "FAILURE") {stat = 'STILL_FAILING'}
+    if (currentBuild?.getPreviousBuild()?.result == 'UNSTABLE' && currentBuild.currentResult == "SUCCESS") {stat = 'BUILD_STABLE'}
+    return stat
+}
+
