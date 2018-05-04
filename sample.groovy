c1902837b501ce39e4f580ae299ddc4398db1436
giddo4all <gideonaina@yahoo.com>
2018-05-03 20:11:40 -0500

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -3,7 +3,16 @@ def slackMsg(){
     echo pwd()
 	    sh ("ls -la")
 	    sh("ls -la ../")
-    slackSend color: '#00FF00', message: 'Sent Finally'
+    slackSend color: '#00FF00', message: getBuildStatus()
 
+	
+def getBuildStatus() {
+    def stat = currentBuild.currentResult
+    if (currentBuild?.getPreviousBuild()?.resultIsWorseOrEqualTo('FAILURE') && currentBuild.currentResult == "SUCCESS") {stat = 'BUILD_FIXED'}
+    if (currentBuild?.getPreviousBuild()?.resultIsWorseOrEqualTo('FAILURE') && currentBuild.currentResult == "FAILURE") {stat = 'STILL_FAILING'}
+    if (currentBuild?.getPreviousBuild()?.result == 'UNSTABLE' && currentBuild.currentResult == "SUCCESS") {stat = 'BUILD_STABLE'}
+    return stat
+}
+	
 }
 return this
